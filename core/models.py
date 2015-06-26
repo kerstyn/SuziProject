80b83f2c7c0765951a474211705957ab1286983e
kerstyn <kerst22@gmail.com>
2015-06-26 13:14:19 +0000

    question model and new HTML for site

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# question or statement posed by user
+class Question(models.Model):
+    author = models.ForeignKey('auth.User')
+    question = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def __str__(self):
+        return self.question
